d3f24912319b9395290b70d9a3f7129b18992a1a
yunyechan9893 <dpcks9893@naver.com>
2023-10-28 14:38:25 +0900

    Study: 데이터 클래스, Map 등을 활용한 쿼리 파라미터

--- src/main/kotlin/com/example/mvc/controller/get/GetApiContoller.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiContoller.kt
@@ -1,8 +1,11 @@
 package com.example.mvc.controller.get
 
+import model.http.UserRequest
+import org.apache.catalina.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController // REST API Contraller 동작
@@ -29,5 +32,38 @@ class GetApiContoller {
     fun pathVariable(@PathVariable name:String): String{
         return "나의 이름은 ${name}입니다"
     }
-    
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name:String, @PathVariable age:Int): String{
+        println("${name}, ${age}")
+        return name + " " + age
+    }
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}")
+    fun pathVariable2(@PathVariable(value="name") _name:String, @PathVariable(name="age") age:Int): String{
+        var name = "yyc"
+        println("${name}, ${age}")
+        return _name + " " + age
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+            @RequestParam name: String,
+            @RequestParam(value="age") age:Int
+    ):String {
+        return name + " " + age
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest{
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any> {
+        println(map)
+        val phoneNumber = map.get("phone-number")
+        print(phoneNumber)
+        return map
+    }
 }
---
+++ src/main/kotlin/model/http/UserRequest.kt
@@ -0,0 +1,8 @@
+package model.http
+
+data class UserRequest (
+        var name:String? = null,
+        var age:Int? = null,
+        var email:String? = null,
+        var address:String? = null
+)
